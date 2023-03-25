f608209c89888a5675f7ce8c5a2697a3c97b1261
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-25 18:05:49 +0530

    created docdb and made changes in parameters to store docdb username and password

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier = "${var.env}-docdb}
+  engine = var.engine
+  master_username = ""
+  master_password = ""
+  backup_retention_period =
+  preferred_backup_windows =
+  skip_final_snapshot =
+}
