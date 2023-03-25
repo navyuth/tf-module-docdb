14eff1dac350fbba1caf20c5d3f29bc5bc115bbc
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-25 18:38:57 +0530

    create subnet group for docdb

--- vars.tf
+++ vars.tf
@@ -1,2 +1,8 @@
 variable "env" {}
 variable "engine" {}
+variable "engine_version" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "skip_final_snapshot" {}
+variable "subnet_ids" {}
+variable "tags" {}
