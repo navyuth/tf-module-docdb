b0718eccec74d25177e1ff02cb6a99290a8c8b49
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-03-25 19:26:51 +0530

    created cluster

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
-#resource "aws_docdb_cluster" "docdb" {
-#  cluster_identifier = "${var.env}-docdb}"
-#  engine = var.engine
-#  engine_version = var.engine_version
-#  master_username = data.aws_ssm_parameter.user.value
-#  master_password = data.aws_ssm_parameter.pass.value
-#  backup_retention_period = var.backup_retention_period
-#  preferred_backup_window = var.preferred_backup_window
-#  skip_final_snapshot = var.skip_final_snapshot
-#}
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier = "${var.env}-docdb}"
+  engine = var.engine
+  engine_version = var.engine_version
+  master_username = data.aws_ssm_parameter.user.value
+  master_password = data.aws_ssm_parameter.pass.value
+  backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
+  skip_final_snapshot = var.skip_final_snapshot
+  db_subnet_group_name = aws_docdb_subnet_group.main.name
+  kms_key_id = data.aws_kms_key.alias.key_id
+}
 
 resource "aws_docdb_subnet_group" "main" {
   name       = "${var.env}-docdb"
